79541816f8733f252fad21790e68be2b54fd5070
Hareesh123NH <162862005+Hareesh123NH@users.noreply.github.com>
2024-08-30 08:54:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 function getFormvalue() {
     //Write your code here
-
+	const form=document.forms["form1"];
+	const fname=form["fname"].value;
+	const lname=form["lname"].value;
+	alert(`First Name: ${fname}\nLast Name: ${lname}`);
 }
